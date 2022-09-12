eba79b75dd7b4b85fa6f3e71b4a00100aa99db07
Ramkrishna Saha <rajsahaking456@gmail.com>
2022-09-13 01:38:25 +0530

    commit for portfolio website

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -25,42 +25,42 @@ function setActiveStyle(color){
 
 // light and dark mode 
 // default mode is dark mode
-const dayNight = document.querySelector(".day-night");
-var image = document.querySelector('#founder-img');
-dayNight.addEventListener("click", () =>{
+// const dayNight = document.querySelector(".day-night");
+// var image = document.querySelector('#founder-img');
+// dayNight.addEventListener("click", () =>{
    
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("light");
-    /* preventing dark mode toggle on refreshing start*/
-    var SetTheme=document.body;
-    var theme;
-    if(SetTheme.classList.contains("light")){
-        theme="LIGHT";
-        image.src = "img/Founder-light.png";
+//     dayNight.querySelector("i").classList.toggle("fa-sun");
+//     dayNight.querySelector("i").classList.toggle("fa-moon");
+//     document.body.classList.toggle("light");
+//     /* preventing dark mode toggle on refreshing start*/
+//     var SetTheme=document.body;
+//     var theme;
+//     if(SetTheme.classList.contains("light")){
+//         theme="LIGHT";
+//         image.src = "img/Founder-light.png";
         
-    }else{
-        theme="DARK";
-        image.src = "img/Founder-dark.png";
-    }
-    localStorage.setItem("PageTheme",JSON.stringify(theme));
-})
+//     }else{
+//         theme="DARK";
+//         image.src = "img/Founder-dark.png";
+//     }
+//     localStorage.setItem("PageTheme",JSON.stringify(theme));
+// })
 
-let GetTheme=JSON.parse(localStorage.getItem("PageTheme"));
+// let GetTheme=JSON.parse(localStorage.getItem("PageTheme"));
 
-if(GetTheme==="LIGHT"){
-    document.body.classList="light";
-}
+// if(GetTheme==="LIGHT"){
+//     document.body.classList="light";
+// }
 
  /* preventing dark mode toggle on refreshing end*/
-window.addEventListener("load", () =>{
-    if(document.body.classList.contains("light")){
-        dayNight.querySelector("i").classList.add("fa-moon");
-        image.src = "img/Founder-light.png";
+// window.addEventListener("load", () =>{
+//     if(document.body.classList.contains("light")){
+//         dayNight.querySelector("i").classList.add("fa-moon");
+//         image.src = "img/Founder-light.png";
       
-    }
-    else{
-        dayNight.querySelector("i").classList.add("fa-sun");
-        image.src = "img/Founder-dark.png";
-    }
-})
+//     }
+//     else{
+//         dayNight.querySelector("i").classList.add("fa-sun");
+//         image.src = "img/Founder-dark.png";
+//     }
+// })
